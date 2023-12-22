2a90db200f8cc39b5b14caef2261b72c7515dec2
ARRARIAKU2 <yerry_kecak@yahoo.com>
2023-12-22 22:31:33 +0700

    Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,12 @@
-import Login from "./Pages/Login";
+
 import Router from "./Routes/routes"
-import { CarsList, CarsCreate, CarsUpdate, CarsDetail } from './Pages/Cars';
 
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { ThemeProvider } from '@mui/material';
-import { theme } from './Config/theme';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <CarsList />,
-  },
-  {
-    path: '/detail/:id',
-    element: <CarsDetail />,
-  },
-  {
-    path: '/create',
-    element: <CarsCreate />,
-  },
-  {
-    path: '/update/:id',
-    element: <CarsUpdate />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-]);
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Router />
-    </ThemeProvider>
+
+    <Router />
+
   );
 
 }
